18da9c88fb61700e4ea03cdf66160dde39a74d9d
linusbehrens <mail@linus-behrens.de>
2025-07-23 21:34:10 +0200

    aufräumen

--- git.pull.fzf.bash
+++ git.pull.fzf.bash
@@ -1,21 +1,18 @@
 #!/bin/bash
 
+exit="▌ exit"
 path=$HOME/.repos
+
 repos=$(ls -1 $path)
-edits=$''
-edits+="▌ exit"
-edits+=$'\n'
-x=1
 cd $path
 
 source_repos () {
     edits=$''
-    edits+="▌ exit"
+    edits+="$exit"
     edits+=$'\n'
 
     for repo in $repos; do
         cd $repo
-        # echo $(pwd)
         echo "$repo"
         result=$(git fetch origin && git diff --name-status HEAD...origin/main --diff-filter=ACMRT)
         if [[ -n $result ]]; then
@@ -27,25 +24,17 @@ source_repos () {
 }
 
 if_exit () {
-    if [[ -z "$edit" || "$edit" == "e" || "$edit" == "▌ exit" ]]; then
-        echo "$edit"
-        exit 0
-        exit 0
-        exit 0
-    fi
-    if [[ "$msg" == "e" || "$msg" == "▌ exit" ]]; then
-        echo "$edit"
-        exit 0
-        exit 0
+    local test=$1
+    if [[ -z "$test" || "$test" == "e" || "$test" == "▌ exit" ]]; then
+        echo "$test"
         exit 0
     fi
-
 }
 
-until [[ "$edits" == "" ]]; do
+while true; do
     source_repos
     edit=$(printf "%s" "$edits" | fzf --tmux 80%,80% --preview 'cd {} && git diff --name-status HEAD...origin/main --diff-filter=ACMRT' --print-query)
-    if_exit
+    if_exit $edit
 
     cd "$path/$edit" || continue
     echo "$edit"
@@ -54,9 +43,11 @@ until [[ "$edits" == "" ]]; do
         --border-label " $edit " \
         --preview "cd \"$path/$edit\" && git diff --color=always -- '{}'" \
         --preview-window=right:70%:wrap \
-        --print-query)
-        # --preview 'git diff --color=always -- "{}"' \
-    if_exit
+        --print-query\
+    )
+
+    if_exit $msg
+
     if [[ -n "$msg" ]]; then
         echo "git pull $edit"
         git pull 
